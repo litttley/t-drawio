ab8d1b9829b8edac51c913b1c62560bd09c4bf1c
littley <617914118@qq.com>
2022-10-16 14:29:23 +0800

    优化app加载

--- src/webapp/js/PreConfig.js
+++ src/webapp/js/PreConfig.js
@@ -11,3 +11,4 @@ window.DRAWIO_LIGHTBOX_URL = null; // Replace with your lightbox URL, eg. https:
 window.DRAW_MATH_URL = 'math/es5';
 window.DRAWIO_CONFIG = null; // Replace with your custom draw.io configurations. For more details, https://www.diagrams.net/doc/faq/configure-diagram-editor
 urlParams['sync'] = 'manual';
+urlParams['lang'] = 'zh';
